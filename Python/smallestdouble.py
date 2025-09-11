8c9c939129008de9ebc6dbc0e7dd94d77a4c87c4
jayakrishnakintali <jayakrishnakintali@gmail.com>
2025-09-11 21:35:27 +0530

    Add files via upload

---
+++ Python/smallestdouble.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+smallest = min(a,b)
+smallestdouble = smallest * 2
+print(f"smallestdouble is {smallestdouble}")
+
+
